0e43d39637019b16b7fab1dac48654db70e4dccf
Fotis Chronopoulos <chrfotis@gmail.com>
2017-05-12 17:33:57 +0300

    remove unused imports

--- djangoPharma/app/views.py
+++ djangoPharma/app/views.py
@@ -4,7 +4,6 @@ Definition of views.
 
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.template import RequestContext
 from datetime import datetime
 
 def home(request):
