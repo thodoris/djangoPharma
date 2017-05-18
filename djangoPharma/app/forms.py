856f03e435f3066552621ddec2140a0eb83f2cf5
Fotis Chronopoulos <chrfotis@gmail.com>
2017-05-18 20:29:33 +0300

    contact info page

--- djangoPharma/app/forms.py
+++ djangoPharma/app/forms.py
@@ -6,8 +6,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
-from registration.forms import  RegistrationForm
-from app.models import  UserAddress
+from registration.forms import RegistrationForm
+from app.models import UserAddress
+
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
@@ -18,22 +19,21 @@ class BootstrapAuthenticationForm(AuthenticationForm):
     password = forms.CharField(label=_("Password"),
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
-                                   'placeholder':'Password'}))
+                                   'placeholder': 'Password'}))
 
 
 class UserForm(RegistrationForm):
-
-    email = forms.EmailField(label=_("Email"),required=True,widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Email'}))
-    first_name = forms.CharField(label=_("Firstname"), required=False,max_length=254,
-                                     widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'First name'}))
-    last_name = forms.CharField(label=_("Lastname"),  required=False,max_length=254,
-                                     widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Last name'}))
+    email = forms.EmailField(label=_("Email"), required=True, widget=forms.TextInput({
+        'class': 'form-control',
+        'placeholder': 'Email'}))
+    first_name = forms.CharField(label=_("Firstname"), required=False, max_length=254,
+                                 widget=forms.TextInput({
+                                     'class': 'form-control',
+                                     'placeholder': 'First name'}))
+    last_name = forms.CharField(label=_("Lastname"), required=False, max_length=254,
+                                widget=forms.TextInput({
+                                    'class': 'form-control',
+                                    'placeholder': 'Last name'}))
 
     class Meta:
         model = User
@@ -43,4 +43,12 @@ class UserForm(RegistrationForm):
 class UserAddressForm(forms.ModelForm):
     class Meta:
         model = UserAddress
-        fields = ('street', 'streetno', 'city','zip')
+        fields = ('street', 'streetno', 'city', 'zip')
+
+
+class ContactForm(forms.Form):
+    contact_email = forms.EmailField(required=True)
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
