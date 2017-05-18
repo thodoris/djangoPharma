c282537367948a6dbd18ab969a98e6f89115b79c
Fotis Chronopoulos <chrfotis@gmail.com>
2017-05-18 20:51:40 +0300

    update add drug form

--- djangoPharma/drugs/forms.py
+++ djangoPharma/drugs/forms.py
@@ -1,9 +1,12 @@
 from django.forms import ModelForm
 from .models import Drug
+from django import forms
 
 
 class AddDrugsForm(ModelForm):
+    # name = forms.CharField(initial='class')
+    days = forms.ChoiceField(choices=[(x, x) for x in range(1, 32)])
+
     class Meta:
         model = Drug
-        fields = ['id', 'friendlyName', 'availability',  'description']
-        managed = False
+        fields = ['id', 'friendlyName', 'availability', 'description']
