2ce6f7a387e0a80f52e7b9612a8643fe457a1d67
Fotis Chronopoulos <chrfotis@gmail.com>
2017-06-09 15:31:44 +0300

    drug details and cart page functionality

--- djangoPharma/app/static/app/scripts/main.js
+++ djangoPharma/app/static/app/scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var url = 'http://localhost:8000/add_to_cart';
+    var url = 'http://localhost:8000';
 
     // add to cart
     $('#addToCart').on('click', function () {
@@ -15,7 +15,7 @@ $(document).ready(function () {
         };
         $.ajax({
             type: "POST",
-            url: url,
+            url: url + '/add_to_cart',
             data: data, beforeSend: function (xmlHTTPRequest) {
                 xmlHTTPRequest.setRequestHeader('X-CSRFToken', token)
             },
@@ -27,6 +27,33 @@ $(document).ready(function () {
             }
         });
     });
+
+      $('#updateCart').on('click', function () {
+        var quantity = $('#quantity').val();
+        var drug_id = $('#drugId').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        // hide the messages
+        hideAlertResultMessages();
+        var data = {
+            quantity: quantity,
+            drug_id: drug_id
+        };
+        $.ajax({
+            type: "POST",
+            url: url + '/update_cart',
+            data: data, beforeSend: function (xmlHTTPRequest) {
+                xmlHTTPRequest.setRequestHeader('X-CSRFToken', token)
+            },
+            success: function () {
+                $('#cart-alert-success').removeClass('hidden')
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                $('#cart-alert-fail').removeClass('hidden')
+            }
+        });
+    });
+
+
 
     function hideAlertResultMessages() {
         $('#cart-alert-success').addClass('hidden');
