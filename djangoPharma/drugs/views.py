f2a80f9258d06c0fbffe0449ae6f8032f73c1b83
Fotis Chronopoulos <chrfotis@gmail.com>
2017-06-12 19:44:39 +0300

    admin customer orders

--- djangoPharma/drugs/views.py
+++ djangoPharma/drugs/views.py
@@ -35,7 +35,7 @@ def __getDrugAsModel(drug_id):
 def __getCategoryChoices():
     # get the drug categories from Cache
     drug_categories = cacheService.get_drug_categories()
-    category_choices = [( int(category['id']), category['name']) for category in drug_categories]
+    category_choices = [(int(category['id']), category['name']) for category in drug_categories]
     return category_choices
 
 
@@ -65,10 +65,9 @@ def test(request):
 
 
 def detail(request, drug_id):
-    # drug = Drug.objects.get(pk=drug_id)
     drug = __getDrugAsModel(drug_id)
     context = {'drug': drug,
-               'image_path': '/static/app/images/drugs/drug_' + drug.imagePath}
+               'image_path': '/static/app/images/drugs/' + drug.imagePath}
     return render(request, 'app/drug_details.html', context)
 
 
@@ -82,12 +81,12 @@ def add_drug(request):
     if request.method == 'GET':
         id_choices = __getDrugIDsChoices()
         category_choices = __getCategoryChoices()
-        form = AddDrugsForm(categorychoices=category_choices,category=None)
+        form = AddDrugsForm(categorychoices=category_choices, category=None)
     elif request.method == 'POST':
         id_choices = __getDrugIDsChoices()
         category_choices = __getCategoryChoices()
 
-        form = AddDrugsForm(request.POST, categorychoices=None,category=None)
+        form = AddDrugsForm(request.POST, categorychoices=None, category=None)
 
         # the function checks also if there is another record with the same id
         if form.is_valid():
@@ -143,8 +142,8 @@ def update_drug(request, drug_id):
             else:
                 # save the model
                 form.save()
-                #update the cache
-                cacheService.get_drug(drug.id , True)
+                # update the cache
+                cacheService.get_drug(drug.id, True)
                 update_succeed = True
         else:
             update_succeed = False
@@ -164,8 +163,9 @@ def manage_migrations(request):
     return HttpResponse(200)
 
 
+# the image for the drug will be one of the static images in the application, random each time
 def get_random_image_for_drug():
-    drug_images_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-                        '13', '14', '15']
+    drug_images_nums = ['000840105', '023280101', '038260201', '041670301', '093360302',
+                        '099880107', '175990402']
     drug_number = random.choice(drug_images_nums)
     return drug_number + '.png'
