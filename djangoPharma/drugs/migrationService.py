947617b4e10772203d158b2eca8e291f3e8d31ed
thodoris <thodoris@thodoris.net>
2017-06-16 21:18:27 +0300

    --search drugs 2

--- djangoPharma/drugs/migrationService.py
+++ djangoPharma/drugs/migrationService.py
@@ -36,14 +36,3 @@ def synchronize_data():
     return False
 
 
-# Method for saving the drug categories in the local database
-def migrate_drug_categories():
-    # get from the WS the categories
-    drug_categories = soapService.get_drug_categories()
-    try:
-        with transaction.atomic():
-            for category in drug_categories:
-                # save the drug category
-                Category.objects.create(id=category['id'], name=category['name'])
-    except Exception:
-        x = 0
