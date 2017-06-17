1bcc59a81f4b4c4e870d2adcb101fbc360b0268f
thodoris <thodoris@thodoris.net>
2017-06-17 19:33:17 +0300

    --fixes

--- djangoPharma/drugs/urls.py
+++ djangoPharma/drugs/urls.py
@@ -5,8 +5,7 @@ from drugs.views import CategoryDrugsList
 from . import views
 
 urlpatterns = [
-    # ex: /drugs/
-    url(r'^$', views.index, name='index'),
+
     # ex: /drugs/5/
     url(r'^(?P<drug_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^category/([\w-]+)/$', CategoryDrugsList.as_view()),
